a48270e9a0a9d4f44c9c34a147ad993de99326dc
namita <patoliya.nh@gmail.com>
2025-11-27 15:29:44 +0530

    this is for make update

--- src/stores/userStore.js
+++ src/stores/userStore.js
@@ -12,6 +12,7 @@ export const useUserStore = defineStore('user', {
     setToken(token) {
       this.token = token
       localStorage.setItem('token', token)
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
     },
     async register(data) {
       const formData = new FormData()
@@ -30,6 +31,7 @@ export const useUserStore = defineStore('user', {
       this.token = null
       this.currentUser = null
       localStorage.removeItem('token')
+      delete api.defaults.headers.common['Authorization']
       router.push('/login')
     },
     async fetchUsers() {
